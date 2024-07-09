4b2216065dce1cfca5c609d8cec328b0762b63bc
yutaka <shigure@refy.net>
2024-07-09 23:52:23 +0900

    ライブラリのスタイルおかしい気がした

--- Samples/IGSample/FrappeGantt/GanttTaskData.cs
+++ Samples/IGSample/FrappeGantt/GanttTaskData.cs
@@ -1,12 +1,41 @@
-﻿namespace FrappeGanttJS.Blazor
+﻿using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
+
+namespace FrappeGanttJS.Blazor
 {
     public class GanttTaskData
     {
+        [JsonIgnore]
         public string? Id { get; set; }
         public string? Name { get; set; }
         public DateTime? Start { get; set; }
         public DateTime? End { get; set; }
         public int? Progress { get; set; }
+        [JsonIgnore]
         public string[]? Dependencies { get; set; }
+
+        [JsonPropertyName("id")]
+        public string? SerializableId
+        {
+            get => SerializeId(Id);
+            set => Id = ParseSerializableId(value);
+        }
+
+        [JsonPropertyName("dependencies")]
+        public string[]? SerializableDependencies
+        {
+            get => Dependencies?.Select(SerializeId).ToArray();
+            set => Dependencies = value?.Select(ParseSerializableId).ToArray();
+        }
+
+        private string? SerializeId(string id) => string.IsNullOrEmpty(id) ? null : "Task_" + id;
+
+        private string? ParseSerializableId(string? id)
+        {
+            if (string.IsNullOrEmpty(id)) return null;
+            if (!id.StartsWith("Task_")) return null;
+            var parsed = id.Substring("Task_".Length);
+            return string.IsNullOrEmpty(parsed) ? null : parsed;
+        }
     }
 }
--- Samples/IGSample/FrappeGantt/wwwroot/main.js
+++ Samples/IGSample/FrappeGantt/wwwroot/main.js
@@ -7,19 +7,15 @@
                     view_mode: "Day",
                     date_format: "YYYY-MM-DD",
                     on_click: function (task) {
-                        console.log(task);
                         dotnetRef?.invokeMethodAsync('OnClick', task);
                     },
                     on_date_change: function (task, start, end) {
-                        console.log(task, start, end);
                         dotnetRef?.invokeMethodAsync('OnDateChange', task, start, end);
                     },
                     on_progress_change: function (task, progress) {
-                        console.log(task, progress);
                         dotnetRef?.invokeMethodAsync('OnProgressChange', task, progress);
                     },
                     on_view_change: function (mode) {
-                        console.log(mode);
                         dotnetRef?.invokeMethodAsync('OnViewChange', mode);
                     },
                 });
