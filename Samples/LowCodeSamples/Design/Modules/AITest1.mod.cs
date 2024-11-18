945d7faa2530a142a891178354d54a6b28c48952
石川達也 <ishikawa-tatsuya@codeer.co.jp>
2024-11-18 19:43:19 +0900

    調整

---
+++ Samples/LowCodeSamples/Design/Modules/AITest1.mod.cs
@@ -0,0 +1,12 @@
+
+void ClearButton_OnClick()
+{
+    Company.Value = null;
+    Name.Value = null;
+    EMail.Value = null;
+    Address.Value = null;
+    Post.Value = null;
+    TEL.Value = null;
+    URL.Value = null;
+    IsMVP.Value = null;
+}
