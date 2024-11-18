238758cd5fae52c07ae91a814990f19207dab115
Libo-2025 <li-bo@codeer.co.jp>
2024-11-18 17:09:11 +0900

    追加：バーコードデモ

---
+++ Samples/LowCodeSamples/Design/Modules/PosSales.mod.cs
@@ -0,0 +1,37 @@
+void DetailLayoutDesign_OnBeforeInitialization()
+{
+    listProduct.AllowLoad = false;
+}
+
+void DetailLayoutDesign_OnAfterInitialization()
+{
+    numOutQty.Value=1;
+    txtBarcodeInput.Focus();
+    
+}
+
+void txtBarcodeInput_OnDataChanged()
+{
+     
+    if(string.IsNullOrEmpty(txtBarcodeInput.Value.Trim()) return;
+    
+    listProduct.AllowLoad = true;
+    
+    txtCode.Value = txtBarcodeInput.Value;
+    var searcher = new ModuleSearcher<PosProducts>();
+    var targetCode = txtBarcodeInput.Value;
+    searcher.AddEquals(e => e.Code.Value, targetCode);
+    
+    listProduct.SetAdditionalCondition(searcher);
+    listProduct.Reload();
+    
+    txtBarcodeInput.Value=string.Empty;
+    
+}
+
+
+void btnClear_OnClick()
+{
+    txtBarcodeInput.Value=string.Empty;
+    txtBarcodeInput.Focus();
+}
