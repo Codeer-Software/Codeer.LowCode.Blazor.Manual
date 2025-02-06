d3cba710003a118e5699ce04a2052c78711e2563
采乃 和田 <wada-ayano@codeer.co.jp>
2025-02-06 18:16:00 +0900

    サンプルサイトビデオ会議修正

---
+++ Samples/LowCodeSamples/Design/Modules/VideoConferencingAccountSetting.mod.cs
@@ -0,0 +1,46 @@
+var sercher = new ModuleSearcher<VideoConferencingAccount>();
+var accountData = sercher.Execute();
+
+void DetailLayoutDesign_OnAfterInitialization()
+{
+    if(LoginUserLink.Value == "" || LoginUserLink.Value == null)
+    {
+        LoginUserLink.Value = "001";
+    }
+    
+}
+
+void LoginUserLink_OnDataChanged()
+{
+    SetAccount();
+}
+
+void SetAccount()
+{
+    foreach(var i in accountData)
+    {
+        if(LoginUserLink.Value == i.LoginAccountLink.Value)
+        {
+            AccountNameText.Value = i.LoginAccountLink.UserName.Value;
+            LoginStatusSelect.Value = i.LoginStatusSelect.Value;
+            LoginStatusBoolean.Value = i.LoginStatusBoolean.Value;
+            ProfileText.Value = i.ProfileText.Value;
+            break;
+        }
+    }
+}
+void LoginStatusBoolean_OnDataChanged()
+{
+    if(LoginStatusBoolean.Value == true)
+    {
+        LoginStatusSelect.IsViewOnly = true;
+    }
+    else
+    {
+        LoginStatusSelect.IsViewOnly = false;
+    }
+}
+void SaveButton_OnClick()
+{
+    Toaster.Error("デモ用のため保存できません");
+}
