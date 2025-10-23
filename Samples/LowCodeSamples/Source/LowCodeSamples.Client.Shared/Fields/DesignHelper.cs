8792fa0d6038651ffb1a33d2e456becd057ed26d
采乃 和田 <wada-ayano@codeer.co.jp>
2025-10-23 17:24:29 +0900

    MeterField追加。ダッシュボード追加

---
+++ Samples/LowCodeSamples/Source/LowCodeSamples.Client.Shared/Fields/DesignHelper.cs
@@ -0,0 +1,101 @@
+﻿using Codeer.LowCode.Blazor.OperatingModel;
+using Codeer.LowCode.Blazor.Repository.Design;
+
+namespace LowCodeSamples.Client.Shared.Fields
+{
+    static class DesignHelper
+    {
+        internal static string? Or(this string? that, string? then) => string.IsNullOrEmpty(that) ? then : that;
+
+        internal static IFontAppearance? GetFontAppearance(this FieldBase field)
+        {
+            switch (field.ModuleLayoutType)
+            {
+                case ModuleLayoutType.List:
+                    {
+                        if (!field.Module.Design.ListLayouts.TryGetValue(field.LayoutName, out var layout)) return null;
+                        return layout.Elements.SelectMany(e => e).FirstOrDefault(e => e.FieldName == field.Design.Name);
+                    }
+                case ModuleLayoutType.Detail:
+                    {
+                        if (!field.Module.Design.DetailLayouts.TryGetValue(field.LayoutName, out var layout)) return null;
+                        return layout.Layout.GetDescendantFields().FirstOrDefault(e => e.FieldName == field.Design.Name);
+                    }
+                case ModuleLayoutType.Search:
+                    {
+                        if (!field.Module.Design.SearchLayouts.TryGetValue(field.LayoutName, out var layout)) return null;
+                        return layout.Layout.GetDescendantFields().FirstOrDefault(e => e.FieldName == field.Design.Name);
+                    }
+                default:
+                    return null;
+            }
+        }
+
+        internal static string? GetBackgroundColor(this FieldBase field)
+        {
+            switch (field.ModuleLayoutType)
+            {
+                case ModuleLayoutType.List:
+                    {
+                        if (!field.Module.Design.ListLayouts.TryGetValue(field.LayoutName, out var layout)) return null;
+                        return layout.Elements.SelectMany(e => e).FirstOrDefault(e => e.FieldName == field.Design.Name)?.BackgroundColor;
+                    }
+                case ModuleLayoutType.Detail:
+                    {
+                        if (!field.Module.Design.DetailLayouts.TryGetValue(field.LayoutName, out var layout)) return null;
+                        return layout.Layout.GetDescendantFields().FirstOrDefault(e => e.FieldName == field.Design.Name)?.BackgroundColor;
+                    }
+                case ModuleLayoutType.Search:
+                    {
+                        if (!field.Module.Design.SearchLayouts.TryGetValue(field.LayoutName, out var layout)) return null;
+                        return layout.Layout.GetDescendantFields().FirstOrDefault(e => e.FieldName == field.Design.Name)?.BackgroundColor;
+                    }
+                default:
+                    return null;
+            }
+        }
+
+        internal static IEnumerable<FieldLayoutDesign> GetDescendantFields(this LayoutDesignBase layout) =>
+            layout.GetDescendantLayouts().OfType<FieldLayoutDesign>();
+
+        internal static IEnumerable<CanvasLayoutDesign> GetDescendantCanvas(this LayoutDesignBase layout) =>
+            layout.GetDescendantLayouts().OfType<CanvasLayoutDesign>();
+
+
+        internal static List<LayoutDesignBase> GetDescendantLayouts(this LayoutDesignBase? layout)
+        {
+            var list = new List<LayoutDesignBase>();
+            if (layout != null) list.Add(layout);
+
+            if (layout is GridLayoutDesign grid)
+            {
+                foreach (var row in grid.Rows)
+                {
+                    foreach (var col in row.Columns)
+                    {
+                        list.AddRange(col.Layout.GetDescendantLayouts());
+                    }
+                }
+            }
+
+            if (layout is TabLayoutDesign tabLayout)
+            {
+                foreach (var tab in tabLayout.Layouts)
+                {
+                    list.AddRange(tab.GetDescendantLayouts());
+                }
+            }
+
+            if (layout is CanvasLayoutDesign canvas)
+            {
+                foreach (var col in canvas.Elements)
+                {
+                    list.AddRange(col.Layout.GetDescendantLayouts());
+                }
+            }
+
+            return list;
+        }
+
+    }
+}
---
+++ Samples/LowCodeSamples/Source/LowCodeSamples.Client.Shared/Fields/MeterField.cs
@@ -0,0 +1,27 @@
+﻿using Codeer.LowCode.Blazor.DataIO;
+using Codeer.LowCode.Blazor.OperatingModel;
+using Codeer.LowCode.Blazor.Repository.Data;
+using Codeer.LowCode.Blazor.Script;
+
+namespace LowCodeSamples.Client.Shared.Fields
+{
+    public class MeterField(MeterFieldDesign design)
+        : FieldBase<MeterFieldDesign>(design)
+    {
+        public double Value { get; private set; }
+
+        public override bool IsModified => false;
+        public override FieldDataBase? GetData() => null;
+        public override FieldSubmitData GetSubmitData() => new();
+        public override async Task InitializeDataAsync(FieldDataBase? fieldDataBase) => await Task.CompletedTask;
+        public override async Task SetDataAsync(FieldDataBase? fieldDataBase) => await Task.CompletedTask;
+
+        [ScriptMethodToProperty("Value")]
+        public void SetValue(double value)
+        {
+            if (Value == value) return;
+            Value = value;
+            NotifyStateChanged();
+        }
+    }
+}
