d30c21922a91da4ff2c487df3d4ea5371990a4e8
采乃 和田 <wada-ayano@codeer.co.jp>
2025-12-19 18:10:45 +0900

    プロコードサンプル追加

---
+++ Samples/LowCodeSamples/Source/LowCodeSamples.Client.Shared/Mapping/Data/q_売上集計.cs
@@ -0,0 +1,11 @@
+﻿using Codeer.LowCode.Blazor.Repository.Data;
+namespace LowCodeSamples.Client.Shared.Mapping.Data
+{
+    public class q_売上集計
+    {
+        public TextFieldData? 商品名 { get; set; }
+        public NumberFieldData? 合計金額 { get; set; }
+        public DateFieldData? 期間開始日 { get; set; }
+        public DateFieldData? 期間終了日 { get; set; }
+    }
+}
---
+++ Samples/LowCodeSamples/Source/LowCodeSamples.Client.Shared/Samples/BubbleList/BubbleListLogic.cs
@@ -0,0 +1,15 @@
+﻿using Codeer.LowCode.Blazor.DataIO;
+using LowCodeSamples.Client.Shared.Mapping.Data;
+
+namespace LowCodeSamples.Client.Shared.Samples.BubbleList
+{
+    public static class BubbleListLogic
+    {
+        public static async Task<List<q_売上集計>> GetSalesSummaryData(this Codeer.LowCode.Blazor.RequestInterfaces.Services services, DateOnly start, DateOnly end)
+        {
+            var list = (await services.ModuleDataService.GetListAsync<q_売上集計>
+                (e => e.期間開始日!.Value == start && e.期間終了日!.Value == end)).ToList();
+            return list ?? new();
+        }
+    }
+}
