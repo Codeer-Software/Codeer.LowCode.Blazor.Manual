cd3bbc475067d02447ec50b7cc3c951a89262bef
采乃 和田 <wada-ayano@codeer.co.jp>
2025-12-22 17:21:06 +0900

    プロコードサンプル調整

--- Samples/LowCodeSamples/Source/LowCodeSamples.Client.Shared/Mapping/Data/q_売上集計.cs
+++ Samples/LowCodeSamples/Source/LowCodeSamples.Client.Shared/Mapping/Data/q_売上集計.cs
@@ -3,6 +3,7 @@ namespace LowCodeSamples.Client.Shared.Mapping.Data
 {
     public class q_売上集計
     {
+        public TextFieldData? 商品コード { get; set; }
         public TextFieldData? 商品名 { get; set; }
         public NumberFieldData? 合計金額 { get; set; }
         public DateFieldData? 期間開始日 { get; set; }
