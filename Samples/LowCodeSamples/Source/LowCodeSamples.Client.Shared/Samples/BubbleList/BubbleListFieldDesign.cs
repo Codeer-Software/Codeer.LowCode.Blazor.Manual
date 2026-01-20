b89e38a16f5b71e2c826245443da08c8eb3ab019
采乃 和田 <wada-ayano@codeer.co.jp>
2026-01-20 19:22:02 +0900

    プロコードサンプル調整

---
+++ Samples/LowCodeSamples/Source/LowCodeSamples.Client.Shared/Samples/BubbleList/BubbleListFieldDesign.cs
@@ -0,0 +1,14 @@
+﻿using Codeer.LowCode.Blazor.OperatingModel;
+using Codeer.LowCode.Blazor.Repository.Data;
+using Codeer.LowCode.Blazor.Repository.Design;
+namespace LowCodeSamples.Client.Shared.Samples.BubbleList
+{
+    public class BubbleListFieldDesign() : FieldDesignBase(typeof(BubbleListFieldDesign).FullName!)
+    {
+        public override string GetWebComponentTypeFullName() => typeof(BubbleListFieldComponent).FullName!;
+        public override string GetSearchWebComponentTypeFullName() => String.Empty;
+        public override string GetSearchControlTypeFullName() => String.Empty;
+        public override FieldBase CreateField() => new BubbleListField(this);
+        public override FieldDataBase? CreateData() => null;
+    }
+}
